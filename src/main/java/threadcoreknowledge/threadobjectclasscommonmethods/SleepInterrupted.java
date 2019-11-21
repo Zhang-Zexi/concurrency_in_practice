b0aa4b5f917e6d0cd53b9e684d200692b0c3b2c9
zhangzx <zhangzx@allinfinance.com>
2019-11-21 19:49:46 +0800

    7-11 sleep相应中断 优雅写法

---
+++ src/main/java/threadcoreknowledge/threadobjectclasscommonmethods/SleepInterrupted.java
@@ -0,0 +1,38 @@
+package threadcoreknowledge.threadobjectclasscommonmethods;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @ClassName SleepInterrupted
+ * @Description 每隔1秒钟输出当前时间，被中断，观察。
+ * Thread.sleep();
+ * TimeUnit.SECONDS.sleep();
+ * @Author zhangzx
+ * @Date 2019/11/21 19:32
+ * Version 1.0
+ **/
+public class SleepInterrupted implements Runnable{
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new SleepInterrupted());
+        thread.start();
+        Thread.sleep(6500);
+        thread.interrupt();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i ++) {
+            System.out.println(new Date());
+            try {
+                TimeUnit.HOURS.sleep(3);
+                TimeUnit.MINUTES.sleep(25);
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                System.out.println("我被中断了！");
+                e.printStackTrace();
+            }
+        }
+    }
+}
