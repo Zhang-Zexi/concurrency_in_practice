38fd5e5842c70e2a775584676a972ecafd52ab99
zhangzx <zhangzx@allinfinance.com>
2019-11-21 10:59:19 +0800

    5-10 使用volatile boolean的方式停止线程 如果阻塞的情况下，线程是无法唤醒中断的，进入wait 所以还是要用interrupted去中断

---
+++ src/main/java/threadcoreknowledge/stopthreads/volatiledemo/WrongWayVolatileFixed.java
@@ -0,0 +1,83 @@
+package threadcoreknowledge.stopthreads.volatiledemo;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+/**
+ * @ClassName WrongWayVolatileFixed
+ * @Description 用中断来修复无尽等待问题
+ * @Author zhangzx
+ * @Date 2019/11/21 10:52
+ * Version 1.0
+ **/
+public class WrongWayVolatileFixed {
+    public static void main(String[] args) throws InterruptedException {
+        WrongWayVolatileFixed body = new WrongWayVolatileFixed();
+        ArrayBlockingQueue storage = new ArrayBlockingQueue(10);
+
+        Producer producer = body.new Producer(storage);
+        Thread productThread = new Thread(producer);
+        productThread.start();
+        Thread.sleep(1000);
+
+        Consumer consumer = body.new Consumer(storage);
+        while (consumer.needMoreNums()) {
+            System.out.println(consumer.storage.take() + "被消费了");
+            Thread.sleep(1000);
+        }
+        System.out.println("消费者不需要更多数据了");
+
+        // 一旦消费不需要更多数据了，我们应该让生产者也停下来，但是实际情况
+//        producer.canceled = true;
+        productThread.interrupt();
+        System.out.println(producer.canceled);
+    }
+
+    class Producer implements Runnable {
+
+        public volatile boolean canceled = false;
+
+        BlockingQueue storage;
+
+        public Producer(BlockingQueue storage) {
+            this.storage = storage;
+        }
+
+        @Override
+        public void run() {
+            int num = 0;
+            try {
+                while (num <= 100000 && !Thread.currentThread().isInterrupted()) {
+                    if (num % 100 == 0) {
+                        storage.put(num);
+                        System.out.println(num + "是100的倍数, 被放到仓库中");
+                    }
+                    num++;
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println("生产者停止运行");
+            }
+        }
+    }
+
+
+
+    class Consumer {
+        BlockingQueue storage;
+
+        public Consumer(BlockingQueue storage) {
+            this.storage = storage;
+        }
+
+        public boolean needMoreNums() {
+            if (Math.random() > 0.95) {
+                return false;
+            }
+            return true;
+        }
+    }
+}
+
+
